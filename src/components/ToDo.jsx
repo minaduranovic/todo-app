7647916e4743791ef381ffc1e501ca751286f5c7
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-10-23 23:35:49 +0200

    almost done

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,14 +1,33 @@
-import { useDebugValue, useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const ToDo = () => {
   const [text, setText] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
   const handleChange = (event) => {
     setText(event.target.value);
   };
+
+  const handleCheckTodoItem = (itemId) => {
+    const itemToCheckIndex = todoList.findIndex((item) => item.id === itemId);
+    const todoListCopy = [...todoList];
+    todoListCopy[itemToCheckIndex].isCompleted = true;
+  }
+  const addItem = () => {
+    const newItem = {
+      id : uuidv4(),
+      text : text,
+      isCompleted: false
+    }
+    setTodoList((prevState) => [...prevState,newItem]); 
+    setText("");
+  };
+
   useEffect(() => {
-    console.log(text);
-  }, [text]);
+    console.log(todoList);
+  }, [todoList]);
+
   return (
     <div>
       <h2> TODO APP </h2>
@@ -16,12 +35,23 @@ const ToDo = () => {
         <input
           type="text"
           className="input"
-          onChange={(event) => handleChange(event)}
+          onChange={handleChange}
           value={text}
         />
-        <button className="add-button"> +Add</button>
+        <button className="add-button" onClick={addItem} disabled={!text} >
+          +Add
+        </button>
+      </div>
+      <div>
+        {todoList.map((todoItem) => (
+          <div  key={todoItem.id} className={`todo-item-wrapper`}>
+          <h3 className={`${todoItem.isCompleted ? 'line-through' : ''}`}> {todoItem.text}</h3>  
+          <button onClick={()=> handleCheckTodoItem(todoItem.id)}>Check</button>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
+
 export default ToDo;
